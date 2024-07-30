750aa767b5aac5c655ddda0eba13d08c9dbcd134
NamNg102 <ngonamhk102@gmail.com>
2024-07-30 10:44:26 +0700

    fixbug image 3d

--- example/src/App.js
+++ example/src/App.js
@@ -76,7 +76,7 @@ export default function App() {
             }}
             debugging={false}
             minFaceSize={0.4}
-            isThreeDimension={false}
+            isThreeDimension={true}
           />
         </View>
       )}
--- ios/LivenessView.swift
+++ ios/LivenessView.swift
@@ -13,117 +13,118 @@ import LocalAuthentication
 
 @available(iOS 13.0, *)
 class LivenessView: UIView, LivenessUtilityDetectorDelegate {
-  var livenessDetector: LivenessUtilityDetector?
-  var debugging = false
-//  var isThreeDimension = false
-  var isDoneSmile = false
+    var livenessDetector: LivenessUtilityDetector?
+    var debugging = false
+    //  var isThreeDimension = false
+    var isDoneSmile = false
     var minFaceSize = 0.25
-  
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-//    setupView()
-  }
- 
-  required init?(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)
-//    setupView()
-  }
- 
-  private func setupView() {
-    // in here you can configure your view
-    Task {
-      do {
-        if isThreeDimension == true {
-          self.livenessDetector = LivenessUtil.createLivenessDetector(previewView: self, threshold: .low,delay: 0, smallFaceThreshold: minFaceSize, debugging: self.debugging, delegate: self, livenessMode: faceIDAvailable ? .threeDimension : .twoDimension)
-        } else {
-          self.livenessDetector = LivenessUtil.createLivenessDetector(previewView: self, threshold: .low,delay: 0, smallFaceThreshold: minFaceSize, debugging: self.debugging, delegate: self, livenessMode: .twoDimension)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        //    setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        //    setupView()
+    }
+    
+    private func setupView() {
+        // in here you can configure your view
+        Task {
+            do {
+                print(isThreeDimension)
+                if isThreeDimension == true {
+                    self.livenessDetector = LivenessUtil.createLivenessDetector(previewView: self, threshold: .low,delay: 0, smallFaceThreshold: minFaceSize, debugging: self.debugging, delegate: self, livenessMode: faceIDAvailable ? .threeDimension : .twoDimension)
+                } else {
+                    self.livenessDetector = LivenessUtil.createLivenessDetector(previewView: self, threshold: .low,delay: 0, smallFaceThreshold: minFaceSize, debugging: self.debugging, delegate: self, livenessMode: .twoDimension)
+                }
+                try self.livenessDetector?.getVerificationRequiresAndStartSession()
+            } catch {
+                pushEvent(data: error)
+            }
         }
-        try self.livenessDetector?.getVerificationRequiresAndStartSession()
-      } catch {
-        pushEvent(data: error)
-      }
     }
-  }
-  
-  private func pushEvent(data: Any) -> Void {
-    if (self.onEvent != nil) {
-      let event = ["data": data]
-      self.onEvent!(event)
-    }
-  }
-  
-  @objc var onEvent: RCTBubblingEventBlock?
     
-  @objc func setDebugging(_ val: Bool) {
-    self.debugging = val as Bool
-  }
-    
-  @objc func setMinFaceSize(_ val: Double) {
-    self.minFaceSize = val
-      self.setupView()
-  }
-
-  @objc var isThreeDimension = false {
-    didSet {
-//      self.setupView()
-    }
-  }
-  
-  func liveness(liveness: LivenessUtilityDetector, didFail withError: LivenessError) {
-    pushEvent(data: withError)
-  }
-  
-  func liveness(liveness: LivenessUtility.LivenessUtilityDetector, didFinish verificationImage: UIImage, thermalImage: UIImage?, color: String?, videoURL: URL?) {
-    Task {
-        let compressedImage = self.compressTo(0.3, image: verificationImage)
-        let image1 = compressedImage?.jpegData(compressionQuality: 0.3)!
-        let livenessImage = image1?.base64EncodedString(options: Data.Base64EncodingOptions.lineLength64Characters)
-      if faceIDAvailable == true {
-        if thermalImage != nil {
-            let compressedImageThermal = self.compressTo(0.3, image: thermalImage!)
-          let image2 = compressedImageThermal?.jpegData(compressionQuality: 0.3)!
-          let thermalImageBase64 = image2?.base64EncodedString(options: Data.Base64EncodingOptions.lineLength64Characters)
-            pushEvent(data: ["message": "done smile", "action": 8, "livenessImage": livenessImage ?? "", "thermalImage": thermalImageBase64 ?? "", "videoURL": videoURL?.absoluteString ?? "", "color": color ?? ""])
-        } else {
-            pushEvent(data: ["message": "done smile", "action": 8, "livenessImage": livenessImage ?? "", "videoURL": videoURL?.absoluteString ?? "", "color": color ?? ""])
+    private func pushEvent(data: Any) -> Void {
+        if (self.onEvent != nil) {
+            let event = ["data": data]
+            self.onEvent!(event)
         }
-      } else {
-        pushEvent(data: ["message": "done smile", "action": 8, "livenessImage": livenessImage ?? "", "videoURL": videoURL?.absoluteString ?? "", "color": color ?? ""])
-      }
-//      pushEvent(data: ["message": "done smile", "action": 8, "livenessImage": livenessImage])
-      livenessDetector?.stopLiveness()
     }
-  }
     
-  func liveness(liveness: LivenessUtilityDetector, startLivenessAction action: LivenessAction) {
-    if action == .smile{
-      isDoneSmile = false
-      pushEvent(data: ["message": "check smile", "action": action.rawValue])
-    } else if action == .fetchConfig{
-      isDoneSmile = false
-      pushEvent(data: ["message": "start check smile", "action": action.rawValue])
-    } else if action == .detectingFace{
-      isDoneSmile = false
-      pushEvent(data: ["message": "detect face", "action": action.rawValue])
-    } else if isDoneSmile == false{
-      isDoneSmile = true
-//      pushEvent(data: ["message": "done smile", "action": action.rawValue])
-      print("done smile")
+    @objc var onEvent: RCTBubblingEventBlock?
+    
+    @objc func setDebugging(_ val: Bool) {
+        self.debugging = val as Bool
     }
-  }
     
-    
-  func stopLiveness() {
-    livenessDetector?.stopLiveness()
-  }
-
-  var faceIDAvailable: Bool {
-    if #available(iOS 11.0, *) {
-      let context = LAContext()
-      return (context.canEvaluatePolicy(LAPolicy.deviceOwnerAuthentication, error: nil) && context.biometryType == .faceID)
+    @objc func setMinFaceSize(_ val: Double) {
+        self.minFaceSize = val
+        self.setupView()
     }
-    return false
-  }
+    
+    @objc var isThreeDimension = false {
+        didSet {
+            //      self.setupView()
+        }
+    }
+    
+    func liveness(liveness: LivenessUtilityDetector, didFail withError: LivenessError) {
+        pushEvent(data: withError)
+    }
+    
+    func liveness(liveness: LivenessUtility.LivenessUtilityDetector, didFinish verificationImage: UIImage, thermalImage: UIImage?, color: String?, videoURL: URL?) {
+        Task {
+            let compressedImage = self.compressTo(0.3, image: verificationImage)
+            let image1 = compressedImage?.jpegData(compressionQuality: 1)!
+            let livenessImage = image1?.base64EncodedString(options: Data.Base64EncodingOptions.lineLength64Characters)
+            if faceIDAvailable == true {
+                if thermalImage != nil {
+                    let compressedImageThermal = self.compressTo(0.3, image: thermalImage!)
+                    let image2 = compressedImageThermal?.jpegData(compressionQuality: 1)!
+                    let thermalImageBase64 = image2?.base64EncodedString(options: Data.Base64EncodingOptions.lineLength64Characters)
+                    pushEvent(data: ["message": "done smile", "action": 8, "livenessImage": livenessImage ?? "", "thermalImage": thermalImageBase64 ?? "", "videoURL": videoURL?.absoluteString ?? "", "color": color ?? ""])
+                } else {
+                    pushEvent(data: ["message": "done smile", "action": 8, "livenessImage": livenessImage ?? "", "videoURL": videoURL?.absoluteString ?? "", "color": color ?? ""])
+                }
+            } else {
+                pushEvent(data: ["message": "done smile", "action": 8, "livenessImage": livenessImage ?? "", "videoURL": videoURL?.absoluteString ?? "", "color": color ?? ""])
+            }
+            //      pushEvent(data: ["message": "done smile", "action": 8, "livenessImage": livenessImage])
+            livenessDetector?.stopLiveness()
+        }
+    }
+    
+    func liveness(liveness: LivenessUtilityDetector, startLivenessAction action: LivenessAction) {
+        if action == .smile{
+            isDoneSmile = false
+            pushEvent(data: ["message": "check smile", "action": action.rawValue])
+        } else if action == .fetchConfig{
+            isDoneSmile = false
+            pushEvent(data: ["message": "start check smile", "action": action.rawValue])
+        } else if action == .detectingFace{
+            isDoneSmile = false
+            pushEvent(data: ["message": "detect face", "action": action.rawValue])
+        } else if isDoneSmile == false{
+            isDoneSmile = true
+            //      pushEvent(data: ["message": "done smile", "action": action.rawValue])
+            print("done smile")
+        }
+    }
+    
+    
+    func stopLiveness() {
+        livenessDetector?.stopLiveness()
+    }
+    
+    var faceIDAvailable: Bool {
+        if #available(iOS 11.0, *) {
+            let context = LAContext()
+            return (context.canEvaluatePolicy(LAPolicy.deviceOwnerAuthentication, error: nil) && context.biometryType == .faceID)
+        }
+        return false
+    }
     
     func compressTo(_ expectedSizeInMb:Double, image: UIImage) -> UIImage? {
         let sizeInBytes = Int(expectedSizeInMb * 1024 * 1024)
@@ -147,5 +148,5 @@ class LivenessView: UIView, LivenessUtilityDetectorDelegate {
         }
         return nil
     }
-
+    
 }
